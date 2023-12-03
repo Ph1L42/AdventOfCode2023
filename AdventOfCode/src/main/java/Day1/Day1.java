8ac0fe03436c92bc7faf745399492544768fcc9e
Philipp <philippphan@gmail.com>
2023-12-03 23:28:38 +0100

    Day One Part One

--- AdventOfCode/src/main/java/Day1/Day1.java
+++ AdventOfCode/src/main/java/Day1/Day1.java
@@ -4,22 +4,30 @@ import java.io.*;
 
 public class Day1 {
 
-    static File inputFile = new File("F:\\PWorkspace\\AdventOfCode2023\\src\\Day1\\TestInput.txt");
-    static int firstNumer;
-    static int secondNumber;
+    static File inputFile = new File("F:\\PWorkspace\\AdventOfCode2023\\AdventOfCode\\src\\main\\java\\Day1\\InputDay1.txt");
     static int sum;
-
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
         String currentLine;
+        String withoutNum;
+        int currentSum = 0;
 
-        while((currentLine = bufferedReader.readLine()) != null){
+        while ((currentLine = bufferedReader.readLine()) != null) {
 
+            withoutNum = currentLine.replaceAll("[a-zA-Z]", "");
+            String firstChar = String.valueOf(withoutNum.charAt(0));
+            String secondChar = String.valueOf(withoutNum.charAt(withoutNum.length() - 1));
+
+            String numString = firstChar + secondChar;
+            currentSum = Integer.parseInt(numString);
+
+            sum += currentSum;
+
+            System.out.println(withoutNum);
         }
-
-
+        System.out.println(sum);
     }
 
 }
