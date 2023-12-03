175a3b2ed8e11de282cd630415422d00f5f2c809
Philipp <philippphan@gmail.com>
2023-12-03 20:58:44 +0100

    Init

---
+++ AdventOfCode/src/main/java/Day1/Day1.java
@@ -0,0 +1,25 @@
+package Day1;
+
+import java.io.*;
+
+public class Day1 {
+
+    static File inputFile = new File("F:\\PWorkspace\\AdventOfCode2023\\src\\Day1\\TestInput.txt");
+    static int firstNumer;
+    static int secondNumber;
+    static int sum;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+        String currentLine;
+
+        while((currentLine = bufferedReader.readLine()) != null){
+
+        }
+
+
+    }
+
+}
